934b5f43baefc0e135c423a51008224242a0a246
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 00:44:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // your code here
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getRepositories() {
